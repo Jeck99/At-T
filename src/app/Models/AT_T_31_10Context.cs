39c7eab849559c91ad19461067e37061ea1df658
Ronniep7 <31841010+Ronniep7@users.noreply.github.com>
2017-10-31 13:21:20 +0200

    models
    adding models

---
+++ src/app/Models/AT_T_31_10Context.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AT_T_31_10.Models
+{
+    public class AT_T_31_10Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public AT_T_31_10Context() : base("name=AT_T_31_10Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<AT_T_31_10.Models.Applicant> Applicants { get; set; }
+
+        public System.Data.Entity.DbSet<AT_T_31_10.Models.Job> Jobs { get; set; }
+
+        public System.Data.Entity.DbSet<AT_T_31_10.Models.Manager> Managers { get; set; }
+
+        public System.Data.Entity.DbSet<AT_T_31_10.Models.Review> Reviews { get; set; }
+
+        public System.Data.Entity.DbSet<AT_T_31_10.Models.ManagerLogins> ManagerLogins { get; set; }
+    }
+}
