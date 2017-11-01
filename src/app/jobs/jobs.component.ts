8d6fb7cb4de72a06022168cff8c390da6d72faea
Ronniep7 <31841010+Ronniep7@users.noreply.github.com>
2017-11-01 18:37:27 +0200

    1/11/17 6:37 Lior

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-jobs',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
-  ngOnInit() {
+  arItems: any[];
+
+  clickHandlerGet() {
+    let req = this.http.get("http://localhost:51210/api/jobs");
+    req.subscribe(rsp => {
+      this.arItems = rsp.json();
+      console.log(this.arItems); ``
+    });
   }
+
 
 }
