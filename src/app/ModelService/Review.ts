f223125aad8636cf71ccdc6ba2275a7ae89e0f68
Jacob <jeck99@gmail.com>
2017-11-28 13:29:39 +0200

    I Love Israel !!!
    I Love Israel

--- src/app/ModelService/Review.ts
+++ src/app/ModelService/Review.ts
@@ -5,4 +5,12 @@
         ManagerId : number;
         Status :  string;
         Content : string;
+        InterviewDate : Date;
+
+        /**
+         *
+         */
+        constructor() {
+            this.InterviewDate = new Date();               
+        }
     }
